77c128e2a2a89ca65fa55123564e03ea3c630f9d
Alexandre Barbosa <xandesbarbosa@gmail.com>
2025-02-19 12:16:55 -0300

    feat: add library shadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductsPage = () => {
+  return (
+    <div className="p-5 border-blue-600">
+      <h1 className="text-red-500">Products Page</h1>
+      <Button>FSW 2.0</Button>
+    </div>
+  );
+};
+
+export default ProductsPage;
